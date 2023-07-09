4c18370b3cd079b6d08eb546a6bbeb801a9934bb
paulaoak <pc2222@ic.ac.uk>
2023-07-09 09:08:23 +0100

    file to source necessary functions for simulation

---
+++ 05-week/simulation-mmax-mean-src/00-simulation-mmax-mean-source.R
@@ -0,0 +1,15 @@
+# Import necessary libraries
+library(quaketools)
+
+# Source necessary functions for the analysis
+source(here::here("05-week", "simulation-mmax-mean-src",
+                  "01-priors"))
+
+source(here::here("05-week", "simulation-mmax-mean-src",
+                  "02-posteriors"))
+
+source(here::here("05-week", "simulation-mmax-mean-src",
+                  "03-mh-step"))
+
+source(here::here("05-week", "simulation-mmax-mean-src",
+                  "04-mcmc"))
