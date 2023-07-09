08645384701b039fc5cc9b7f6f37c2ff0a158c87
paulaoak <pc2222@ic.ac.uk>
2023-07-09 11:01:48 +0100

    source functions

--- 05-week/simulation-mmax-mean-src/00-simulation-mmax-mean-source.R
+++ 05-week/simulation-mmax-mean-src/00-simulation-mmax-mean-source.R
@@ -3,13 +3,13 @@ library(quaketools)
 
 # Source necessary functions for the analysis
 source(here::here("05-week", "simulation-mmax-mean-src",
-                  "01-priors"))
+                  "01-priors.R"))
 
 source(here::here("05-week", "simulation-mmax-mean-src",
-                  "02-posteriors"))
+                  "02-posteriors.R"))
 
 source(here::here("05-week", "simulation-mmax-mean-src",
-                  "03-mh-step"))
+                  "03-mh-step.R"))
 
 source(here::here("05-week", "simulation-mmax-mean-src",
-                  "04-mcmc"))
+                  "04-mcmc.R"))
