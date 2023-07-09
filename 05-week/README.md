658ef40d3d8b64dfa6a6160a726815fede374003
paulaoak <pc2222@ic.ac.uk>
2023-07-09 08:20:45 +0100

    priors for mmax mean simulation

---
+++ 05-week/README.md
@@ -0,0 +1,6 @@
+Simulations for a GPD distribution under the Mmax and mean reparameterisation.
+This reparameterisation has several advantages over the usual shape and scale
+parameterisation in terms of interpretability. This can be of great help when
+working with experts to elicit priors on the parameters (Bayesian setting).
+Furthermore, the Mmax and mean parameters are less correlated than the 
+shape and scale parameters, which can be of help when performing inference.
---
+++ 05-week/simulation-mmax-mean-src/01-priors.R
@@ -0,0 +1,51 @@
+###########################
+# LOG-PRIORS
+###########################
+
+# Import libraries
+library(quaketools)
+
+#Define the different uninformative prior functions for the Mmax and mean
+#parameters of a GPD distribution
+
+
+###########################
+# CONSTANT THRESHOLD
+###########################
+
+#Uniform prior on mean parameter between threshold and b_value approximation +
+#an epsilon error and the mmax paramter uniform prior between the mean and an
+#upper bound for mmax
+#It is important to notice that in this case mmax and mean are not independent
+#unlike when we specified a prior on the shape and scale parameters
+
+unif_log_prior <- function(params, threshold, upper_mmax, b_value, epsilon) {
+  mmax <- params[1]
+  mean <- params[2]
+
+  stopifnot(b_value + epsilon < upper_mmax)
+
+  if(mean >= mmax){
+    log_prior <- -1e7
+  }
+  else{
+    prior <- 1/((upper_mmax - threshold)^2 - (upper_mmax - b_value - epsilon)^2)
+    log_prior <- log(prior)
+  }
+
+  return(log_prior)
+}
+
+gamma_unif_log_prior <- function(params, threshold, b_value, epsilon, alpha, beta) {
+  mmax <- params[1]
+  mean <- params[2]
+
+  if((mmax <= mean) | (b_value > epsilon)){
+    log_prior <- -1e7
+  }
+  else{
+    log_prior <- -log(b_value + epsilon - threshold) + dgamma(mmax - mean, shape = alpha, rate = beta, log = TRUE)
+  }
+
+  return(log_prior)
+}
