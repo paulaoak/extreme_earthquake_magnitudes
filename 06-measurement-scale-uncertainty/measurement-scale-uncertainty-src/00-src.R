36ae866adc5a58ca692bdf78cc259e872cac25ed
paulaoak <pc2222@ic.ac.uk>
2023-07-22 10:43:22 +0100

    file to source functions

---
+++ 06-measurement-scale-uncertainty/measurement-scale-uncertainty-src/00-src.R
@@ -0,0 +1,16 @@
+# Import necessary libraries
+library(quaketools)
+library(cooltools)
+
+# Source necessary functions for the analysis
+source(here::here("06-measurement-scale-uncertainty", "measurement-scale-uncertainty-src",
+                  "01-likelihood-scale-uncertainty.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "measurement-scale-uncertainty-src",
+                  "02-mle-scale-uncertainty.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "measurement-scale-uncertainty-src",
+                  "03-profile-likelihood-scale.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "measurement-scale-uncertainty-src",
+                  "04-contours-profile-likelihood.R"))
