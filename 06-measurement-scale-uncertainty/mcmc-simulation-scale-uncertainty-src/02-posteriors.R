e54bcb143c1cdf4371d9b3555695aa3fe723b611
paulaoak <pc2222@ic.ac.uk>
2023-07-22 18:17:06 +0100

    rename functions

--- 06-measurement-scale-uncertainty/mcmc-simulation-scale-uncertainty-src/02-posteriors.R
+++ 06-measurement-scale-uncertainty/mcmc-simulation-scale-uncertainty-src/02-posteriors.R
@@ -5,7 +5,7 @@
 ###########################
 # UNDER PENULTIMATE APPROXIMATION
 ###########################
-log_posterior_gpd <- function(params, c, x, u, prior) {
+log_posterior_gpd_scale_penultimate <- function(params, c, x, u, prior) {
   new_params <- c(params, c)
   log_posterior <- llh_gpd_scale_penultimate(sigxi_lambda_c = new_params, u = u, x = x)
   + prior(params)
@@ -17,7 +17,7 @@ log_posterior_gpd <- function(params, c, x, u, prior) {
 ###########################
 # UNDER THE ASSUMPTION THAT GPD MODEL IS VALID FOR DIFFERENT LAMBDAS
 ###########################
-log_posterior_gpd <- function(params, x, u, prior) {
+log_posterior_gpd_scale <- function(params, x, u, prior) {
   log_posterior <- llh_gpd_scale(sigxi_lambda = params, u = u, x = x)
   + prior(params)
 
