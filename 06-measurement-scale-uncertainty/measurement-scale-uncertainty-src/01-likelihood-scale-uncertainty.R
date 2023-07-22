102d73f6ef129f47ae4b0a40a3d3a998a3d88cc5
paulaoak <pc2222@ic.ac.uk>
2023-07-22 09:13:07 +0100

    update likelihood and profile likelihood

--- 06-measurement-scale-uncertainty/measurement-scale-uncertainty-src/01-likelihood-scale-uncertainty.R
+++ 06-measurement-scale-uncertainty/measurement-scale-uncertainty-src/01-likelihood-scale-uncertainty.R
@@ -60,7 +60,7 @@ llh_gpd_scale_penultimate <- function(sigxi_lambda_c, u, x, negative = FALSE){
 
 #Profile likelihood function given lambda for the threshold exceedance model under
 #penultimate approximation
-profile_llh_gpd_scale_penultimate <- function(sigxi, lambda, u, x, negative = FALSE){
+profile_llh_gpd_scale_penultimate <- function(sig, xi, lambda, u, x, negative = FALSE){
 
   # Check inputs
   stopifnot(exprs = {
@@ -68,15 +68,16 @@ profile_llh_gpd_scale_penultimate <- function(sigxi, lambda, u, x, negative = FA
     length(u) == 1
     is.numeric(lambda)
     length(lambda) == 1
-    is.numeric(sigxi)
-    length(sigxi)==2 #unique shape, scale_u, lambda and c parameters
+    is.numeric(sig)
+    length(sig)==1
+    is.numeric(xi)
+    length(xi)==1
     is.logical(negative)
   })
 
   # GPD parameters
-  sig <- sigxi[1]
   sig_y <- sig * u ^(lambda-1)
-  xi_y <- sigxi[2]
+  xi_y <- xi
 
   if(lambda==0){
     x_mod <- log(x)
@@ -97,7 +98,7 @@ profile_llh_gpd_scale_penultimate <- function(sigxi, lambda, u, x, negative = FA
   #check all scale parameters are positive
   condition_1 <- sig <= 0
   #check all x below upper end-point (UEP) (if it exists)
-  condition_2 <- (xi < 0) && (max(x) >= (u - sig / xi))
+  condition_2 <- (xi_y < 0) && (max(x_mod) >= (u_mod - sig_y / xi_y))
 
   if(condition_1 || condition_2){
     llh <- -10e6
@@ -110,7 +111,55 @@ profile_llh_gpd_scale_penultimate <- function(sigxi, lambda, u, x, negative = FA
   return((-1)^negative * llh)
 }
 
+#Maximum likelihood function given for profile likelihood
+llh_gpd_scale_penultimate_for_profile <- function(sigxi_lambda, u, x, negative = FALSE){
 
+  # Check inputs
+  stopifnot(exprs = {
+    is.numeric(u)
+    length(u) == 1
+    is.numeric(sigxi_lambda)
+    length(sigxi_lambda)==3
+    is.logical(negative)
+  })
+
+  # GPD parameters
+  lambda <- sigxi_lambda[3]
+  sig <- sigxi_lambda[1]
+  sig_y <- sig * u ^(lambda-1)
+  xi_y <- sigxi_lambda[2]
+
+  if(lambda==0){
+    x_mod <- log(x)
+    u_mod <- log(u)
+  }
+  else{
+    x_mod <- (x^(lambda)-1)/lambda
+    u_mod <- (u^(lambda)-1)/lambda
+  }
+
+  # Check x all be above u
+  lep_fail <- any(x < u)
+  if(lep_fail){stop('Lower endpoint failure:  !all(x > u).')}
+
+  # Function body
+
+  # Satisfy conditions to be a valid GPD distribution:
+  #check all scale parameters are positive
+  condition_1 <- sig <= 0
+  #check all x below upper end-point (UEP) (if it exists)
+  condition_2 <- (xi_y < 0) && (max(x_mod) >= (u_mod - sig_y / xi_y))
+
+  if(condition_1 || condition_2){
+    llh <- -10e6
+    return((-1)^negative * llh)}
+
+  # Log-likelihood
+  prob <- x^(lambda-1) * dgpd(x = x_mod, shape = xi_y, scale = sig_y, shift = u_mod)
+  llh <- sum(log(prob))
+
+  return((-1)^negative * llh)
+}
 
 #Likelihood function under the assumption that the GP tail form applies for a
 #range of choices of measurement scale lambda
