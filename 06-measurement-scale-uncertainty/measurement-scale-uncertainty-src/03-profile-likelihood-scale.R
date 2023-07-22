e32034b094daa818248a472c0182e4eb88aa5dd7
paulaoak <pc2222@ic.ac.uk>
2023-07-22 09:38:59 +0100

    value of profile likelihood for xi and lambda

---
+++ 06-measurement-scale-uncertainty/measurement-scale-uncertainty-src/03-profile-likelihood-scale.R
@@ -0,0 +1,111 @@
+#############################################
+#MLE when parametrically accounting for uncertainty in the measurement scale
+#############################################
+
+
+#MLE for the threshold exceedance model under penultimate approximation
+profile_mle_c <- function(u, x, sigxi, lambda_min_grid, lambda_max_grid, step_grid, llh_val = TRUE, hessian = FALSE, method =  "Nelder-Mead", maxit = 10000, ...){
+
+  # Check inputs
+  stopifnot(exprs = {
+    is.numeric(u)
+    length(u) == 1
+    is.numeric(lambda_min_grid)
+    length(lambda_min_grid) == 1
+    is.numeric(lambda_max_grid)
+    length(lambda_max_grid) == 1
+    is.numeric(step_grid)
+    length(step_grid) == 1
+    is.numeric(sigxi)
+    length(sigxi)==2
+    is.logical(llh_val)
+    is.logical(hessian)
+  }
+  )
+
+  # Create the grid of lambda values
+  lambda_grid <- seq(lambda_min_grid, lambda_max_grid, by = step_grid)
+
+  # GPD parameters
+  sig_0 <- sigxi[1]
+  xi_0 <- sigxi[2]
+
+  # Check valid starting point
+  condition <- (xi_0 < 0) && (max(x) >= (u - sig_0 / xi_0))
+  if(condition){stop('Invalid starting point. Data above upper end point of distribution.')}
+  stopifnot(sig_0>0)
+
+  # Pre-allocate space to vector with profile MLE estimations of xi_y
+  out <- numeric(length(lambda_grid))
+
+  for(i in 1:length(lambda_grid)){
+    # Numerically minimise negative profile log-likelihood for fixed lambda
+    estimate <- optim(fn = profile_llh_gpd_scale_penultimate,
+                      par = sigxi,
+                      u = u,
+                      x = x,
+                      lambda = lambda,
+                      negative = TRUE,
+                      hessian = hessian,
+                      method = method,
+                      control = list(maxit = maxit, ...))
+    if(estimate$convergence)
+      warning("optimization may not have succeeded")
+    # Format output
+    out <-  estimate$par[2]
+  }
+  return(out)
+}
+
+
+# Estimate c using profile log-likelihood
+#MLE for the threshold exceedance model under penultimate approximation
+profile_mle_xi_lambda <- function(u, x, xi_lambda, sig, method =  "Nelder-Mead", maxit = 10000, ...){
+
+  # Check inputs
+  stopifnot(exprs = {
+    is.numeric(u)
+    length(u) == 1
+    is.numeric(xi_lambda)
+    length(xi_lambda)==2
+  }
+  )
+
+  # GPD parameters
+  xi_y <- xi_lambda[1]
+  lambda <- xi_lambda[2]
+  sig_y_0 <- sig * u^(lambda-1)
+
+  if(lambda==0){
+    x_mod <- log(x)
+    u_mod <- log(u)
+  }
+  else{
+    x_mod <- (x^(lambda)-1)/lambda
+    u_mod <- (u^(lambda)-1)/lambda
+  }
+
+  # Check valid starting point
+  condition <- (xi_y < 0) && (max(x_mod) >= (u_mod - sig_y_0 / xi_y))
+  if(condition){stop('Invalid starting point. Data above upper end point of distribution.')}
+  stopifnot(sig_y_0>0)
+
+  # Numerically minimise negative profile log-likelihood for fixed lambda
+  estimate <- optim(fn = profile_llh_gpd_scale_penultimate,
+                    par = sig,
+                    u = u,
+                    x = x,
+                    xi = xi_y,
+                    lambda = lambda,
+                    negative = TRUE,
+                    hessian = FALSE,
+                    method = method,
+                    control = list(maxit = maxit, ...))
+  if(estimate$convergence)
+    warning("optimization may not have succeeded")
+
+  # Format output
+  out <-  estimate$value
+
+  return(out)
+}
