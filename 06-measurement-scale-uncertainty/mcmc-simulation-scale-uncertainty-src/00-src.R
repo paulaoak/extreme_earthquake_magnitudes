e28a5b73c37c53ae003a1c94d58779677f185f24
paulaoak <pc2222@ic.ac.uk>
2023-07-22 19:22:09 +0100

    source functions

---
+++ 06-measurement-scale-uncertainty/mcmc-simulation-scale-uncertainty-src/00-src.R
@@ -0,0 +1,20 @@
+# Import necessary libraries
+
+# Source necessary functions for the analysis
+source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+                  "01-priors.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+                  "02-posteriors.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+                  "03-mh-step.R"))
+
+#source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+#                  "04-contours-profile-likelihood.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+                  "04-mcmc.R"))
+
+source(here::here("06-measurement-scale-uncertainty", "mcmc-simulation-scale-uncertainty-src",
+                  "05-simulation.R"))
