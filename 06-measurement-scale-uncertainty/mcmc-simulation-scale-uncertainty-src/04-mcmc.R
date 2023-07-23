ab1d3699cb4a5ce7e1ded69569601cdfdadc094c
paulaoak <pc2222@ic.ac.uk>
2023-07-23 11:36:50 +0100

    correct

--- 06-measurement-scale-uncertainty/mcmc-simulation-scale-uncertainty-src/04-mcmc.R
+++ 06-measurement-scale-uncertainty/mcmc-simulation-scale-uncertainty-src/04-mcmc.R
@@ -53,7 +53,7 @@ bayesian_estimation_gpd_scale_penultimate <- function(sigxi_lambda_initial, c, x
 ###########################
 # GPD ACCOUNTING FOR ROUNDING UNDER ASSUMPTION OF VALIDITY OF GPD FOR DIFFERENT LAMBDAS
 ###########################
-bayesian_estimation_gpd_scale <- function(sigxi_lambda_initial, x, u, n_iter, n_burn, sd_xi, sd_sigma, min_lamda, max_lambda, prior_choice = c('flat', 'mdi', 'jeffreys')){
+bayesian_estimation_gpd_scale <- function(sigxi_lambda_initial, x, u, n_iter, n_burn, sd_xi, sd_sigma, min_lambda, max_lambda, prior_choice = c('flat', 'mdi', 'jeffreys')){
 
   prior_choice <- match.arg(prior_choice)
   if(prior_choice == 'flat'){
